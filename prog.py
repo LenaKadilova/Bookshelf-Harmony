3942f9516079eaeb14a0baad5671dc9562a64a42
maruuskin <152617352+maruuskin@users.noreply.github.com>
2024-04-25 23:00:17 +0300

    hotels+restaurants

--- prog.py
+++ prog.py
@@ -19,28 +19,77 @@ async def start(update, context):
 
 
 async def hotels_in_city(update, context):
-    city = context.args[0]
-    print(city)
-    search_api_server = "https://search-maps.yandex.ru/v1/"
-    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+    try:
+        city = context.args[0]
+        print(city)
+        search_api_server = "https://search-maps.yandex.ru/v1/"
+        api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-    search_params = {
-        "apikey": api_key,
-        "text": "отель+в+" + city,
-        "lang": "ru_RU",
-        "type": "biz"
-    }
+        search_params = {
+            "apikey": api_key,
+            "text": "отель+в+" + city,
+            "lang": "ru_RU",
+            "type": "biz"
+        }
 
-    response = requests.get(search_api_server, params=search_params)
+        response = requests.get(search_api_server, params=search_params).json()
+        print(response)
 
-    if response.status_code == 200:
-        data = response.json()
-        hotels = [item['properties']['CompanyMetaData']['name'] for item in data['features']]
-        hotels_list = '\n'.join(hotels)
-
-        await update.message.reply_text(f'Гостиницы в городе {city}:\n{hotels_list}')
-    else:
+        hotels = 'Гостиницы в городе ' + city + '\n'
+        cnt = 0
+        for hotel in response['features']:
+            cnt += 1
+            hotel_name = hotel['properties']['CompanyMetaData'].get('name', '')
+            hotel_address = hotel['properties']['CompanyMetaData'].get('address', '')
+            hotel_url = hotel['properties']['CompanyMetaData'].get('url', '')
+            if hotel_name:
+                hotels += (str(cnt) + ') ' + 'Название: ' + hotel_name + '\n')
+                if hotel_address:
+                    hotels += ('Адрес: ' + hotel_address + '\n')
+                if hotel_url:
+                    hotels += ('Сайт: ' + hotel_url + '\n')
+                hotels += '\n'
+        await update.message.reply_text(hotels)
+    except Exception:
         await update.message.reply_text('Не удалось получить информацию о гостиницах.')
+
+
+async def restaurants(update, context):
+    try:
+        search_api_server = "https://search-maps.yandex.ru/v1/"
+        api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+        address_ll = "37.588392,55.734036"
+
+        search_params = {
+            "apikey": api_key,
+            "text": "кафе",
+            "lang": "ru_RU",
+            "ll": address_ll,
+            "type": "biz"
+        }
+
+        response = requests.get(search_api_server, params=search_params).json()
+        cnt = 0
+        cafes = 'Рестораны недалеко от Вас:\n'
+        for cafe in response['features']:
+            cnt += 1
+            cafe_name = cafe['properties']['CompanyMetaData'].get('name', '')
+            cafe_address = cafe['properties']['CompanyMetaData'].get('address', '')
+            cafe_url = cafe['properties']['CompanyMetaData'].get('url', '')
+            cafe_hours = cafe['properties']['CompanyMetaData'].get('Hours', '').get('text', '')
+            if cafe_name:
+                cafes += (str(cnt) + ') ' + 'Название: ' + cafe_name + '\n')
+                if cafe_address:
+                    cafes += ('Адрес: ' + cafe_address + '\n')
+                if cafe_url:
+                    cafes += ('Сайт: ' + cafe_url + '\n')
+                if cafe_hours:
+                    cafes += ('График работы: ' + cafe_hours + '\n')
+                cafes += '\n'
+        await update.message.reply_text(cafes)
+    except Exception:
+        await update.message.reply_text('Не удалось получить информацию о ресторанах.')
 
 
 def main():
@@ -59,6 +108,7 @@ def main():
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("hotels", hotels_in_city))
+    application.add_handler(CommandHandler("restaurants", restaurants))
     # application.add_handler(text_handler)
 
     # Запускаем приложение.
