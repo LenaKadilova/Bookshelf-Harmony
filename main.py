77a374615db5776af3d0aa91be31eeb82663035f
maruuskin <152617352+maruuskin@users.noreply.github.com>
2024-04-28 17:22:15 +0300

    +карта

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import requests
 import logging
 import random
 import time
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Application, MessageHandler, filters, CommandHandler, Updater
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, Updater, ConversationHandler
 from config import BOT_TOKEN
 import requests
 import json
@@ -13,7 +12,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-
+all_coords = []
 
 
 async def start(update, context):
@@ -21,7 +20,7 @@ async def start(update, context):
         "Доброго времени суток тебе, человек)) Я бот-путешественник, и я помогу сделать твою поездку незабываемой!")
     time.sleep(1)
     await update.message.reply_text(
-        "Что я умею?\nКоманда /weather <название_города> позволяет узнать погоду в указанном городе.\nКоманда /cafes <название_города> дает рекомендации ресторанов в указанном городе.\nКоманда /hotels <название_города> выдает информацию об отелях в указанном городе.\nКоманда /sights <название_города> позволяет узнать больше о достопримечательностях в указанном городе и об их расположении на карте.")
+        "Что я умею?\n\nКоманда /weather <название_города> позволяет узнать погоду в указанном городе.\n\nКоманда /cafes <название_города> дает рекомендации ресторанов в указанном городе.\n\nКоманда /hotels <название_города> выдает информацию об отелях в указанном городе.\n\nКоманда /sights <название_города> позволяет узнать больше о достопримечательностях в указанном городе и об их расположении на карте.\n\nВАЖНО! Я бот-патриот, поэтому хорошо разбираюсь только в тех городах, которые находятся в России.")
 
 
 async def hotels_in_city(update, context):
@@ -57,7 +56,7 @@ async def hotels_in_city(update, context):
                 hotels += '\n'
         await update.message.reply_text(hotels)
     except Exception:
-        await update.message.reply_text('Не удалось получить информацию о гостиницах.')
+        await update.message.reply_text('Не удалось получить информацию о гостиницах. Проверь название города. Он точно находится в России?')
 
 
 async def restaurants(update, context):
@@ -93,14 +92,20 @@ async def restaurants(update, context):
                 cafes += '\n'
         await update.message.reply_text(cafes)
     except Exception:
-        await update.message.reply_text('Не удалось получить информацию о ресторанах.')
+        await update.message.reply_text('Не удалось получить информацию о ресторанах. Проверь название города. Он точно находится в России?')
 
 
 async def sights_in_city(update, context):
+    print(555)
+    global all_coords
+    print(66)
     try:
         search_api_server = "https://search-maps.yandex.ru/v1/"
         api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-        city = context.args[0]
+        # print(context.args)
+        # city = context.args[0]
+        city = 'Чебоксары'
+        print(context)
 
         search_params = {
             "apikey": api_key,
@@ -116,6 +121,8 @@ async def sights_in_city(update, context):
         cnt = 0
         all_coords = []
         sights = 'Достопримечательности в городе ' + city + '\n'
+        coords_left = []
+        coords_right = []
         for sight in response['features']:
             cnt += 1
             sight_name = sight['properties']['CompanyMetaData'].get('name', '')
@@ -123,6 +130,8 @@ async def sights_in_city(update, context):
             sight_coords = sight['geometry']['coordinates']
             # all_coords.append(','.join([str(i) for i in sight_coords[::-1]]))
             all_coords.append(sight_coords)
+            coords_left.append(sight_coords[0])
+            coords_right.append(sight_coords[1])
             if sight_name:
                 sights += (str(cnt) + ') ' + 'Название: ' + sight_name + '\n')
                 if sight_address:
@@ -131,21 +140,40 @@ async def sights_in_city(update, context):
         # all_coords = ','.join(all_coords)
         print(all_coords)
         metks = ''
-        req = "http://static-maps.yandex.ru/1.x/?ll=" + str(all_coords[0][0]) + ',' + str(all_coords[0][1]) + '&spn=0.08,0.08&l=sat&pt=' + str(all_coords[0][0]) + ',' + str(all_coords[0][1]) + ',' + 'pmorl1'
+        req = "http://static-maps.yandex.ru/1.x/?ll=" + str(round((min(coords_left) + max(coords_left)) / 2, 6)) + ',' + str(round((min(coords_right) + max(coords_right)) / 2, 6)) + '&spn=0.2,0.2&l=map&pt=' + str(all_coords[0][0]) + ',' + str(all_coords[0][1]) + ',' + 'pmorl1'
         k = 0
+        print(req)
         print(all_coords)
         for coord in all_coords:
             k += 1
             metks += '~' + str(coord[0]) + ',' + str(coord[1]) + ',' + 'pmorl' + str(k)
         req += metks
-
-        with open('im.png', 'wb') as f:
+        print(sights)
+        with open('im.jpg', 'wb') as f:
             f.write(requests.get(req).content)
+        print(7)
+        # await update.message.reply_text(sights)
+        print(len(sights))
+        # await update.message.reply_photo(photo="im.jpg", caption=sights[:1025])
         await update.message.reply_text(sights)
-        # await update.message.send_photo(photo="im.png", caption="hello test123")
+        await update.message.reply_photo(photo="im.jpg", caption='Введи номер/номера достопримечательностей, которые ты хотел бы посетить, через пробел.\nНапример, 1 3 10')
+        return 1
+
     except Exception as ex:
         print(ex)
-        await update.message.reply_text('Не удалось получить информацию о ресторанах.')
+        await update.message.reply_text('Не удалось получить информацию о достопримечательностях. Проверь название города. Он точно находится в России?')
+
+
+async def sights_numbers(update, context):
+    print(11111)
+    await update.message.reply_text(update.message.text)
+    return ConversationHandler.END  # Константа, означающая конец диалога.
+    # Все обработчики из states и fallbacks становятся неактивными.
+
+
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
 
 
 async def weather_response(update, context):
@@ -165,7 +193,8 @@ async def weather_response(update, context):
         t_feels = 'Ощущается как ' + str(temperature_feels) + ' °C.'
         t_desc = 'В городе ' + city + ' сейчас ' + weather_data['weather'][0]['description'] + '.'
         await update.message.reply_text(t_now + '\n' + t_feels + '\n' + t_desc)
-    except Exception:
+    except Exception as ex:
+        print(ex)
         await update.message.reply_text("Проверьте название города!")
 
 
@@ -179,16 +208,33 @@ def main():
     # После регистрации обработчика в приложении
     # эта асинхронная функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
-    # text_handler = MessageHandler(filters.TEXT, start)
+    text_handler = MessageHandler(filters.TEXT, sights_in_city)
 
     # Регистрируем обработчик в приложении.
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("hotels", hotels_in_city))
     application.add_handler(CommandHandler("cafes", restaurants))
-    application.add_handler(CommandHandler("sights", sights_in_city))
     application.add_handler(CommandHandler("weather", weather_response))
-    # application.add_handler(text_handler)
+    application.add_handler(text_handler)
+
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('sights', sights_in_city)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, sights_numbers)]
+        },
+
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    application.add_handler(conv_handler)
 
     # Запускаем приложение.
     application.run_polling()
