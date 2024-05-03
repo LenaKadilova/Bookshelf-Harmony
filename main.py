ec48961ca4a18d47d60307f5001c6a8d661e7fe3
maruuskin <152617352+maruuskin@users.noreply.github.com>
2024-05-03 12:16:04 +0300

    help + pep8

--- main.py
+++ main.py
@@ -12,24 +12,26 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-all_coords = []
 
 
+# БД для хранения информации о пользователях и городах, про достопримечательности в которых он спрашивали
 def create_database():
-    print(111111111111111)
     conn = sqlite3.connect('cities.db')
     cursor = conn.cursor()
+
     cursor.execute("""CREATE TABLE IF NOT EXISTS users_cities (
                         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                         user_name TEXT,
                         city_id INTEGER,
                         FOREIGN KEY(city_id) REFERENCES cities(city_id)
                     )""")
+
     cursor.execute("""CREATE TABLE IF NOT EXISTS cities (
                         city_id INTEGER PRIMARY KEY AUTOINCREMENT,
                         city_name TEXT,
                         UNIQUE(city_id)
                     )""")
+
     cursor.execute("""CREATE TABLE IF NOT EXISTS sights (
                             sight_id INTEGER PRIMARY KEY AUTOINCREMENT,
                             sight_name TEXT, 
@@ -37,49 +39,43 @@ def create_database():
                             coords1 DOUBLE,
                             coords2 DOUBLE
                         )""")
+
     conn.commit()
     conn.close()
 
 
+# Добавление информации в БД
 def add_sights_to_db(name, city, sights_names, sights_coords):
     try:
-        print(1111)
         conn = sqlite3.connect('cities.db')
         cursor = conn.cursor()
-        print(1112)
+
         cursor.execute("SELECT city_id FROM cities WHERE city_name=?",
                        (city,))
         city_id = cursor.fetchone()
+
         if not city_id:
             cursor.execute("INSERT INTO cities (city_name) VALUES (?)",
-                           (city.lower().capitalize(), ))
+                           (city.lower().capitalize(),))
             conn.commit()
 
             cursor.execute("SELECT city_id FROM cities WHERE city_name=?",
                            (city,))
             city_id = cursor.fetchone()
 
-        print(555)
-
-        cursor.execute("SELECT user_name FROM users_cities WHERE user_name=?", (name, ))
+        cursor.execute("SELECT user_name FROM users_cities WHERE user_name=?", (name,))
         all_names = cursor.fetchall()
 
-        print(22)
-        print(all_names)
         if not all_names:
             cursor.execute("INSERT INTO users_cities (user_name, city_id) VALUES (?, ?)",
                            (name, city_id[0]))
             conn.commit()
         else:
-            print(city_id[0], name)
             cursor.execute("UPDATE users_cities SET city_id=? WHERE user_name=?",
                            (city_id[0], name))
             conn.commit()
 
-        print(78)
-
         for numb in range(len(sights_names)):
-            print(sights_names[numb], city_id[0], sights_coords[numb][0], sights_coords[numb][1])
             cursor.execute("SELECT sight_id FROM sights WHERE sight_name=? AND city_id=? AND coords1=? AND coords2=?",
                            (sights_names[numb], city_id[0], sights_coords[numb][0], sights_coords[numb][1]))
             same_sights = cursor.fetchall()
@@ -88,20 +84,39 @@ def add_sights_to_db(name, city, sights_names, sights_coords):
                                (sights_names[numb], city_id[0], sights_coords[numb][0], sights_coords[numb][1]))
                 conn.commit()
 
-        print(68)
-
     except Exception as ex:
         print(ex)
 
 
 async def start(update, context):
+    # Вывод приветственного сообщения
     await update.message.reply_text(
         "Доброго времени суток тебе, человек)) Я бот-путешественник, и я помогу сделать твою поездку незабываемой!")
+
     time.sleep(1)
+
+    # Информация о том, что умеет бот
     await update.message.reply_text(
-        "Что я умею?\n\nКоманда /weather <название_города> позволяет узнать погоду в указанном городе.\n\nКоманда /cafes <название_города> дает рекомендации ресторанов в указанном городе.\n\nКоманда /hotels <название_города> выдает информацию об отелях в указанном городе.\n\nКоманда /sights <название_города> позволяет узнать больше о достопримечательностях в указанном городе и об их расположении на карте.\n\nВАЖНО! Я бот-патриот, поэтому хорошо разбираюсь только в тех городах, которые находятся в России.")
+        "Что я умею?\n\nКоманда /weather <название_города> позволяет узнать погоду в указанном городе.\n\nКоманда "
+        "/cafes <название_города> дает рекомендации ресторанов в указанном городе.\n\nКоманда /hotels <название_города>"
+        " выдает информацию об отелях в указанном городе.\n\nКоманда /sights <название_города> позволяет узнать больше"
+        " о достопримечательностях в указанном городе и об их расположении на карте.\n\nКоманда /help напомпит о том, "
+        "что я умею.\n\nВАЖНО! Я бот-патриот, поэтому хорошо разбираюсь только в тех городах, которые находятся в "
+        "России.")
 
 
+async def help(update, context):
+    # Информация о том, что умеет бот
+    await update.message.reply_text(
+        "Что я умею?\n\nКоманда /weather <название_города> позволяет узнать погоду в указанном городе.\n\nКоманда "
+        "/cafes <название_города> дает рекомендации ресторанов в указанном городе.\n\nКоманда /hotels <название_города>"
+        " выдает информацию об отелях в указанном городе.\n\nКоманда /sights <название_города> позволяет узнать больше"
+        " о достопримечательностях в указанном городе и об их расположении на карте."
+        "\n\nВАЖНО! Я бот-патриот, поэтому хорошо разбираюсь только в тех городах, которые находятся в "
+        "России.")
+
+
+# Сбор информации об отелях
 def find_hotels(response):
     hotels = ''
     cnt = 0
@@ -120,14 +135,14 @@ def find_hotels(response):
     return hotels
 
 
+# Отели
 async def hotels_in_city(update, context):
-    print(292929)
     try:
         search_api_server = "https://search-maps.yandex.ru/v1/"
         api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
+        # Если задан город
         if context.args:
-            print(777)
             city = context.args[0]
             search_params = {
                 "apikey": api_key,
@@ -139,8 +154,8 @@ async def hotels_in_city(update, context):
             response = requests.get(search_api_server, params=search_params).json()
             await update.message.reply_text('Гостиницы в городе ' + city + '\n' + find_hotels(response))
 
+        # Если город не задан
         else:
-            print(272727)
             reply_keyboard = [[{"request_location": True, "text": "Где я?"}]]
             markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
             # выводим приветственное сообщение
@@ -153,8 +168,8 @@ async def hotels_in_city(update, context):
             'Не удалось получить информацию о гостиницах. Проверь название города. Он точно находится в России?')
 
 
+# Отели по местоположению пользователя
 async def get_location_hotels(update, context):
-    print(555)
     search_api_server = "https://search-maps.yandex.ru/v1/"
     api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
     current_pos = (update.message.location.latitude, update.message.location.longitude)
@@ -169,12 +184,11 @@ async def get_location_hotels(update, context):
     response = requests.get(search_api_server, params=search_params).json()
     await update.message.reply_text('Отели недалеко от Вас:\n' + find_hotels(response))
 
-    print(current_pos)
     return ConversationHandler.END
 
 
+# Сбор информации о ресторанах
 def find_cafes(response):
-    print(343434)
     cnt = 0
     cafes = ''
     for cafe in response['features']:
@@ -182,7 +196,11 @@ def find_cafes(response):
         cafe_name = cafe['properties']['CompanyMetaData'].get('name', '')
         cafe_address = cafe['properties']['CompanyMetaData'].get('address', '')
         cafe_url = cafe['properties']['CompanyMetaData'].get('url', '')
-        cafe_hours = cafe['properties']['CompanyMetaData'].get('Hours', '').get('text', '')
+        cafe_hours = cafe['properties']['CompanyMetaData'].get('Hours', '')
+        if cafe_hours:
+            cafe_hours = cafe_hours.get('text', '')
+        else:
+            cafe_hours = ''
         if cafe_name:
             cafes += (str(cnt) + ') ' + 'Название: ' + cafe_name + '\n')
             if cafe_address:
@@ -196,13 +214,10 @@ def find_cafes(response):
 
 
 async def restaurants(update, context):
-    print(787878)
     try:
         search_api_server = "https://search-maps.yandex.ru/v1/"
         api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-        print(4440)
         if context.args:
-            print(777)
             city = context.args[0]
             search_params = {
                 "apikey": api_key,
@@ -212,15 +227,17 @@ async def restaurants(update, context):
             }
 
             response = requests.get(search_api_server, params=search_params).json()
+
             await update.message.reply_text('Рестораны в городе ' + city + ':\n' + find_cafes(response))
+
         else:
-            print(666)
             reply_keyboard = [[{"request_location": True, "text": "Где я?"}]]
             markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
             # выводим приветственное сообщение
             await update.message.reply_text(
                 f'''Укажите свое местоположение''', reply_markup=markup)
-            # time.sleep(3)
+
             return 1
 
     except Exception as ex:
@@ -230,7 +247,6 @@ async def restaurants(update, context):
 
 
 async def get_location_cafes(update, context):
-    print(232323)
     search_api_server = "https://search-maps.yandex.ru/v1/"
     api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
     current_pos = (update.message.location.latitude, update.message.location.longitude)
@@ -245,18 +261,13 @@ async def get_location_cafes(update, context):
     response = requests.get(search_api_server, params=search_params).json()
     await update.message.reply_text('Рестораны недалеко от Вас:\n' + find_cafes(response))
 
-    print(current_pos)
     return ConversationHandler.END
 
 
 async def sights_in_city(update, context):
-    print(555)
-    global all_coords
-    print(66)
     try:
         search_api_server = "https://search-maps.yandex.ru/v1/"
         api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-        print(context.args)
         city = context.args[0]
         name = update.message.from_user.username
         print(context)
@@ -271,51 +282,45 @@ async def sights_in_city(update, context):
         with open('jsons.json', 'w') as f:
             json.dump(response, f, indent=4)
 
-        print(response)
         cnt = 0
         all_coords = []
         sights_names = []
         sights = 'Достопримечательности в городе ' + city + '\n'
         coords_left = []
         coords_right = []
+
         for sight in response['features']:
             cnt += 1
             sight_name = sight['properties']['CompanyMetaData'].get('name', '')
             sight_address = sight['properties']['CompanyMetaData'].get('address', '')
             sight_coords = sight['geometry']['coordinates']
-            # all_coords.append(','.join([str(i) for i in sight_coords[::-1]]))
+
             all_coords.append(sight_coords)
             coords_left.append(sight_coords[0])
             coords_right.append(sight_coords[1])
+
             if sight_name:
                 sights += (str(cnt) + ') ' + 'Название: ' + sight_name + '\n')
                 sights_names.append(sight_name)
                 if sight_address:
                     sights += ('Адрес: ' + sight_address + '\n')
                 sights += '\n'
-        # all_coords = ','.join(all_coords)
-        print(all_coords)
+
         metks = ''
         req = "http://static-maps.yandex.ru/1.x/?ll=" + str(
             round((min(coords_left) + max(coords_left)) / 2, 6)) + ',' + str(
             round((min(coords_right) + max(coords_right)) / 2, 6)) + '&spn=0.2,0.2&l=map&pt=' + str(
             all_coords[0][0]) + ',' + str(all_coords[0][1]) + ',' + 'pmorl1'
         k = 0
-        print(req)
-        print(all_coords)
+
         for coord in all_coords:
             k += 1
             metks += '~' + str(coord[0]) + ',' + str(coord[1]) + ',' + 'pmorl' + str(k)
         req += metks
-        print(sights)
+
         with open('im.jpg', 'wb') as f:
             f.write(requests.get(req).content)
-        print(7)
 
-
-        # await update.message.reply_text(sights)
-        print(len(sights))
-        # await update.message.reply_photo(photo="im.jpg", caption=sights[:1025])
         await update.message.reply_text(sights)
         await update.message.reply_photo(photo="im.jpg",
                                          caption='Введи номер/номера достопримечательностей, которые ты хотел бы посетить, через пробел.\nНапример, 1 3 10')
@@ -331,7 +336,6 @@ async def sights_in_city(update, context):
 
 
 async def sights_numbers(update, context):
-    print(11111)
     numbers = update.message.text.split()
     name = update.message.from_user.username
     conn = sqlite3.connect('cities.db')
@@ -343,8 +347,6 @@ async def sights_numbers(update, context):
     cursor.execute("SELECT * FROM sights WHERE city_id=?",
                    (city_id,))
     city_info = cursor.fetchall()
-    print('info')
-    print(city_info)
 
     for number in numbers:
         sight = city_info[int(number) - 1][1]
@@ -366,11 +368,9 @@ async def stop(update, context):
 
 
 async def weather_response(update, context):
-    # await update.message.reply_text("Тут могла бы быть погода")
     try:
         # получаем город из сообщения пользователя
         city = context.args[0]
-        print(city)
         # формируем запрос и отправляем его на сервер
         url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
         weather_data = requests.get(url).json()
@@ -388,8 +388,7 @@ async def weather_response(update, context):
 
 
 def main():
-    # Создаём объект Application.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    # Создаём объект Application
     application = Application.builder().token(BOT_TOKEN).build()
 
     # Создаём обработчик сообщений типа filters.TEXT
@@ -397,21 +396,12 @@ def main():
     # После регистрации обработчика в приложении
     # эта асинхронная функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
-    # text_handler = MessageHandler(filters.TEXT, sights_in_city)
 
     # Регистрируем обработчик в приложении.
 
-    # hotels_location_handler = MessageHandler(filters._Location(), get_location_hotels)
-    # application.add_handler(hotels_location_handler)
-    #
-    # cafes_location_handler = MessageHandler(filters._Location(), get_location_cafes)
-    # application.add_handler(cafes_location_handler)
-
     application.add_handler(CommandHandler("start", start))
-    # application.add_handler(CommandHandler("hotels", hotels_in_city))
-    # application.add_handler(CommandHandler("cafes", restaurants))
+    application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("weather", weather_response))
-    # application.add_handler(text_handler)
 
     conv_handler_sights = ConversationHandler(
         # Точка входа в диалог.
